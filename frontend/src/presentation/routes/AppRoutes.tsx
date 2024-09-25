2856ca934a300801740420db7c88299c87eb54e9
njxue <ngjxue@gmail.com>
2024-09-25 21:19:59 +0800

    Add 404 page

--- frontend/src/presentation/routes/AppRoutes.tsx
+++ frontend/src/presentation/routes/AppRoutes.tsx
@@ -2,22 +2,18 @@ import React from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 import Layout from "presentation/components/Layout";
 import QuestionsPage from "presentation/pages/QuestionsPage";
+import NotFound from "presentation/pages/NotFound";
 
 const AppRoutes: React.FC = () => {
-	return (
-		<Routes>
-			<Route path="/" element={<Navigate to="/questions" />} />
-
-			<Route
-				path="/questions"
-				element={
-					<Layout>
-						<QuestionsPage />
-					</Layout>
-				}
-			/>
-		</Routes>
-	);
+    return (
+        <Layout>
+            <Routes>
+                <Route path="/" element={<Navigate to="/questions" />} />
+                <Route path="/questions" element={<QuestionsPage />} />
+                <Route path="*" element={<NotFound />} />
+            </Routes>
+        </Layout>
+    );
 };
 
 export default AppRoutes;
