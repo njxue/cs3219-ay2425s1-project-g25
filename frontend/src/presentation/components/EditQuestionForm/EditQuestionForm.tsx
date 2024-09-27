baec5dad5ec6fea00fe3f460653b4a4591435283
njxue <ngjxue@gmail.com>
2024-09-27 15:16:10 +0800

    Fix UI issues

--- frontend/src/presentation/components/EditQuestionForm/EditQuestionForm.tsx
+++ frontend/src/presentation/components/EditQuestionForm/EditQuestionForm.tsx
@@ -1,8 +1,5 @@
 import { Input, Form, Select, Row, Col, Button } from "antd";
-import {
-    categoryOptions,
-    difficultyOptions,
-} from "presentation/utils/QuestionUtils";
+import { categoryOptions, difficultyOptions } from "presentation/utils/QuestionUtils";
 import MdEditor from "@uiw/react-md-editor";
 import styles from "../NewQuestionForm/NewQuestionForm.module.css";
 import { Question } from "domain/entities/Question";
@@ -11,43 +8,36 @@ import { questionRepository } from "data/repositories/QuestionRepositoryImpl";
 import { QUESTION_FORM_FIELDS } from "presentation/utils/constants";
 import { questionUseCases } from "domain/usecases/QuestionUseCases";
 import { toast } from "react-toastify";
+import { useEffect } from "react";
 
 interface EditQuestionFormProps {
     question: Question;
-    onSubmit?: () => void;
+    onSubmit?: (updatedQuestion: Question) => void;
 }
-export const EditQuestionForm: React.FC<EditQuestionFormProps> = ({
-    question,
-    onSubmit,
-}) => {
+export const EditQuestionForm: React.FC<EditQuestionFormProps> = ({ question, onSubmit }) => {
     const [form] = Form.useForm();
     const validateMessages = {
         required: "${label} is required",
-        whitespace: "${label} is required",
+        whitespace: "${label} is required"
     };
 
     //  Remove questionId to convert to IQuestionUpdateInput
     const { questionId: _, ...questionUpdateInput } = question;
 
-    const {
-        FIELD_TITLE,
-        FIELD_DIFFICULTY,
-        FIELD_DESCRIPTION,
-        FIELD_CATEGORIES,
-        FIELD_URL,
-    } = QUESTION_FORM_FIELDS;
+    useEffect(() => {
+        form.setFieldsValue(questionUpdateInput);
+    }, [question]);
+
+    const { FIELD_TITLE, FIELD_DIFFICULTY, FIELD_DESCRIPTION, FIELD_CATEGORIES, FIELD_URL } = QUESTION_FORM_FIELDS;
 
     async function handleSubmit(questionUpdate: IQuestionUpdateInput) {
         try {
-            const res = await questionUseCases.updateQuestion(
-                question?.questionId,
-                questionUpdate
-            );
+            const res = await questionUseCases.updateQuestion(question?.questionId, questionUpdate);
             const status = res?.status;
             const data = res?.data;
             if (status === 200) {
                 toast.success(data?.message);
-                onSubmit?.();
+                onSubmit?.(data?.updatedQuestion);
             } else {
                 console.error(data?.message);
                 toast.error(data?.message);
@@ -65,9 +55,10 @@ export const EditQuestionForm: React.FC<EditQuestionFormProps> = ({
                 initialValues={questionUpdateInput}
                 validateMessages={validateMessages}
                 scrollToFirstError
+                form={form}
             >
-                <Row>
-                    <Col span={24}>
+                <Row gutter={16}>
+                    <Col span={20}>
                         <Form.Item
                             label={FIELD_TITLE.label}
                             name={FIELD_TITLE.name}
@@ -77,32 +68,29 @@ export const EditQuestionForm: React.FC<EditQuestionFormProps> = ({
                             <Input placeholder={FIELD_TITLE.label} />
                         </Form.Item>
                     </Col>
-                </Row>
-                <Row gutter={16}>
-                    <Col span={6}>
+                    <Col span={4}>
                         <Form.Item
                             label={FIELD_DIFFICULTY.label}
                             name={FIELD_DIFFICULTY.name}
                             rules={[
                                 {
-                                    required: true,
-                                },
+                                    required: true
+                                }
                             ]}
                         >
-                            <Select
-                                placeholder={FIELD_DIFFICULTY.label}
-                                options={difficultyOptions}
-                            />
+                            <Select placeholder={FIELD_DIFFICULTY.label} options={difficultyOptions} />
                         </Form.Item>
                     </Col>
-                    <Col span={18}>
+                </Row>
+                <Row gutter={16}>
+                    <Col span={16}>
                         <Form.Item
                             label={FIELD_CATEGORIES.label}
                             name={FIELD_CATEGORIES.name}
                             rules={[
                                 {
-                                    required: true,
-                                },
+                                    required: true
+                                }
                             ]}
                         >
                             <Select
@@ -113,13 +101,8 @@ export const EditQuestionForm: React.FC<EditQuestionFormProps> = ({
                             ></Select>
                         </Form.Item>
                     </Col>
-                </Row>
-                <Row>
-                    <Col span={24}>
-                        <Form.Item
-                            label={FIELD_URL.label}
-                            name={FIELD_URL.name}
-                        >
+                    <Col span={8}>
+                        <Form.Item label={FIELD_URL.label} name={FIELD_URL.name}>
                             <Input type="text" placeholder={FIELD_URL.label} />
                         </Form.Item>
                     </Col>
@@ -134,15 +117,10 @@ export const EditQuestionForm: React.FC<EditQuestionFormProps> = ({
                         >
                             <MdEditor
                                 value={form.getFieldValue("descripton") || ""}
-                                onChange={(description) =>
-                                    form.setFieldValue(
-                                        FIELD_DESCRIPTION.name,
-                                        description
-                                    )
-                                }
+                                onChange={(description) => form.setFieldValue(FIELD_DESCRIPTION.name, description)}
                                 overflow={false}
-                                height={500}
                                 enableScroll
+                                height={300}
                             />
                         </Form.Item>
                     </Col>
