84e20fd3a7939167353fdfe25d3c7806c38e4f3a
Whalejay <WhaleJay@outlook.sg>
2024-10-02 21:53:41 +0800

    Switch to nginx

--- backend/api-gateway/src/config/index.ts
+++
@@ -1,8 +0,0 @@
-import dotenv from 'dotenv';
-
-dotenv.config();
-
-export const config = {
-    port: process.env.PORT || 8080,
-    baseServiceUrl: process.env.BASE_SERVICE_URL || 'http://localhost:3001',
-};
