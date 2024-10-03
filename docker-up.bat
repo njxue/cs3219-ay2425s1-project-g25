66c41d92975aefa97dd1860c34633d2e5a1b4dc8
Whalejay <WhaleJay@outlook.sg>
2024-10-03 18:26:31 +0800

    Add user and api service to the root docker composer

--- docker-up.bat
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Navigate to api-gateway directory and run docker-compose in the background
-cd backend/api-gateway
-docker-compose -f docker-compose.api-gateway.yml up --build &
-
-# Wait for the docker-compose process to finish
-wait
