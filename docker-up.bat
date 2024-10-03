ca014349ce5b4e49c71e2bffb9c80b1b15cad318
SPWwj <wei_dev@outlook.com>
2024-10-03 15:47:26 +0800

    rename the docker file to more specific name base on the project

--- docker-up.bat
+++ docker-up.bat
@@ -2,8 +2,7 @@
 
 # Navigate to api-gateway directory and run docker-compose in the background
 cd backend/api-gateway
-docker-compose up --build &
+docker-compose -f docker-compose.api-gateway.yml up --build &
 
-
-# Wait for both docker-compose processes to finish
+# Wait for the docker-compose process to finish
 wait
