198013e8dc0e3edd5363f0f2b2fc4ef35e6da714
njxue <ngjxue@gmail.com>
2024-10-13 22:07:19 +0800

    Remove temporary link to questions page

--- frontend/src/presentation/pages/HomePage.tsx
+++ frontend/src/presentation/pages/HomePage.tsx
@@ -1,19 +1,13 @@
 import styles from "./HomePage.module.css";
 import React from "react";
 import { FindPeerButton } from "presentation/components/buttons/FindPeerButton";
-import { Button } from "antd";
 import { CategoriesDropdown } from "presentation/components/CategoriesDropdown";
 import { DifficultiesDropdown } from "presentation/components/DifficultiesDropdown";
 import { ProfileContainer } from "presentation/components/ProfileContainer";
-import { Tooltip } from "antd";
-import { InfoCircleOutlined } from "@ant-design/icons";
 import { RecentAttemptsTable } from "presentation/components/RecentAttemptsTable";
-import { useNavigate } from "react-router-dom";
 import { CustomTooltip } from "presentation/components/common/CustomTooltip";
 
 const HomePage: React.FC = () => {
-    const navigate = useNavigate();
-
     return (
         <div className={styles.container}>
             <div className={styles.leftContainer}>
@@ -30,9 +24,6 @@ const HomePage: React.FC = () => {
             <div className={styles.rightContainer}>
                 <ProfileContainer />
                 <RecentAttemptsTable />
-                <Button onClick={() => navigate("/questions")} type="primary" className={styles.button}>
-                    Go to questions
-                </Button>
             </div>
         </div>
     );
