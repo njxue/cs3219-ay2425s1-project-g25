ed255320d59eb0acf98e4366cb9333e1681b989c
njxue <ngjxue@gmail.com>
2024-10-13 21:19:54 +0800

    Add custom tooltip

--- frontend/src/presentation/pages/HomePage.tsx
+++ frontend/src/presentation/pages/HomePage.tsx
@@ -1,14 +1,15 @@
-import styles from './HomePage.module.css';
+import styles from "./HomePage.module.css";
 import React from "react";
-import { FindPeerButton } from 'presentation/components/buttons/FindPeerButton';
+import { FindPeerButton } from "presentation/components/buttons/FindPeerButton";
 import { Button } from "antd";
-import { CategoriesDropdown } from 'presentation/components/CategoriesDropdown';
-import { DifficultiesDropdown } from 'presentation/components/DifficultiesDropdown';
-import { ProfileContainer } from 'presentation/components/ProfileContainer';
-import { Tooltip } from 'antd';
-import { InfoCircleOutlined } from '@ant-design/icons';
-import { RecentAttemptsTable } from 'presentation/components/RecentAttemptsTable';
-import { useNavigate } from 'react-router-dom';
+import { CategoriesDropdown } from "presentation/components/CategoriesDropdown";
+import { DifficultiesDropdown } from "presentation/components/DifficultiesDropdown";
+import { ProfileContainer } from "presentation/components/ProfileContainer";
+import { Tooltip } from "antd";
+import { InfoCircleOutlined } from "@ant-design/icons";
+import { RecentAttemptsTable } from "presentation/components/RecentAttemptsTable";
+import { useNavigate } from "react-router-dom";
+import { CustomTooltip } from "presentation/components/common/CustomTooltip";
 
 const HomePage: React.FC = () => {
     const navigate = useNavigate();
@@ -22,17 +23,19 @@ const HomePage: React.FC = () => {
                     <CategoriesDropdown />
                     <DifficultiesDropdown />
                 </div>
-                <Tooltip className={styles.tooltip} title="You will be matched with a user who has selected the same difficulty level as you">
-                    <InfoCircleOutlined style={{ fontSize: '16px', color: '#1890ff', cursor: 'pointer' }} />
-                </Tooltip>
+                <div className={styles.tooltip}>
+                    <CustomTooltip title="You will be matched with a user who has selected the same difficulty level as you" />
+                </div>
             </div>
             <div className={styles.rightContainer}>
                 <ProfileContainer />
                 <RecentAttemptsTable />
-                <Button onClick={() => navigate('/questions')} type="primary" className={styles.button}>Go to questions</Button>
+                <Button onClick={() => navigate("/questions")} type="primary" className={styles.button}>
+                    Go to questions
+                </Button>
             </div>
         </div>
-    )
-}
+    );
+};
 
 export default HomePage;
