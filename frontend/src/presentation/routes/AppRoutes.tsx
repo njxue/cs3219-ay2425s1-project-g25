d00966c2f671abefee5dc33ea1d5b528463273fc
Whalejay <WhaleJay@outlook.sg>
2024-10-20 05:54:25 +0800

    Update folder name

--- frontend/src/presentation/routes/AppRoutes.tsx
+++ frontend/src/presentation/routes/AppRoutes.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
-import Layout from "presentation/pages/layout/Layout";
+import Layout from "presentation/pages/layoutx/Layout";
 import QuestionManagement from "presentation/pages/QuestionManagement";
 import QuestionPage from "presentation/pages/QuestionSelection";
 import NotFound from "presentation/pages/NotFound";
