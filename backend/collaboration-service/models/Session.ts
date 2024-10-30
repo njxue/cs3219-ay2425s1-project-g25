c129c610011aaafad7deb0005b5a97abdb3dbef9
SilasTSL <silastaysl@gmail.com>
2024-10-30 22:06:36 +0800

    Update Session schema

--- backend/collaboration-service/models/Session.ts
+++ backend/collaboration-service/models/Session.ts
@@ -1,8 +1,10 @@
 import mongoose, { Types } from "mongoose";
 
 const sessionSchema = new mongoose.Schema({
-    roomId: { type: Types.ObjectId, required: true },
-    yDocId: { type: String, required: true },
+    matchId: { type: String, required: true },
+    sessionId: { type: String, required: true },
+    codeContent: { type: String, default: "" },
+    lastUpdated: { type: Date, default: Date.now }
 });
 
 const Session = mongoose.model('session', sessionSchema);
