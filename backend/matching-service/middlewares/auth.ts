df683585868f87d35f6155f3ecbbc5975fc0e374
njxue <ngjxue@gmail.com>
2024-11-03 20:17:40 +0800

    Resolve conflicts

--- backend/matching-service/middlewares/auth.ts
+++ backend/matching-service/middlewares/auth.ts
@@ -1,21 +1,22 @@
-import { Request, Response, NextFunction } from 'express';
-import jwt from 'jsonwebtoken';
+import { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
 
 export interface AuthenticatedRequest extends Request {
   userId?: string;
 }
 
 export const authenticateToken = (req: AuthenticatedRequest, res: Response, next: NextFunction) => {
-  const authHeader = req.headers['authorization'];
+  const authHeader = req.headers["authorization"];
 
-  if (!authHeader) return res.status(401).json({ error: 'Authorization header missing' });
+  if (!authHeader) return res.status(401).json({ error: "Authorization header missing" });
 
-  const token = authHeader.split(' ')[1];
+  const token = authHeader.split(" ")[1];
 
-  if (!token) return res.status(401).json({ error: 'Token missing' });
+  if (!token) return res.status(401).json({ error: "Token missing" });
 
   jwt.verify(token, process.env.JWT_ACCESS_TOKEN_SECRET as string, (err, decoded: any) => {
-    if (err) return res.status(403).json({ error: 'Invalid token' });
+    console.log(err);
+    if (err) return res.status(403).json({ error: "Invalid token" });
 
     req.userId = decoded.id;
     next();
